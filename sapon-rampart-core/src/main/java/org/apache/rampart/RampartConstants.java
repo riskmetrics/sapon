8e57bc228526af96f3217615253baf9ffd747fd5
Jason Fager <jason.fager@riskmetrics.com>
2009-11-23 13:31:36 -0500

    add sapon-rampart-core

---
+++ sapon-rampart-core/src/main/java/org/apache/rampart/RampartConstants.java
@@ -0,0 +1,9 @@
+package org.apache.rampart;
+
+public class RampartConstants {
+		
+	public static final String TIME_LOG = "org.apache.rampart.TIME";
+	public static final String MESSAGE_LOG = "org.apache.rampart.MESSAGE";
+	public static final String SEC_FAULT = "SECURITY_VALIDATION_FAILURE";
+
+}
