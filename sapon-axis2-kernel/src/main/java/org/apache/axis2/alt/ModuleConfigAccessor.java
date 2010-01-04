d6485079049ff19ca225be3c1031f7e666573a6a
Jason Fager <jason.fager@riskmetrics.com>
2010-01-04 15:34:59 -0500

    add addModuleConfig method, and TODO about getting rid of this interface

--- sapon-axis2-kernel/src/main/java/org/apache/axis2/alt/ModuleConfigAccessor.java
+++ sapon-axis2-kernel/src/main/java/org/apache/axis2/alt/ModuleConfigAccessor.java
@@ -2,6 +2,8 @@ package org.apache.axis2.alt;
 
 import org.apache.axis2.description.ModuleConfiguration;
 
+//TODO:  this probably shouldn't be an interface on its own.
 public interface ModuleConfigAccessor {
 	ModuleConfiguration getModuleConfig(String moduleName);
+	void addModuleConfig(ModuleConfiguration moduleConfiguration);
 }
