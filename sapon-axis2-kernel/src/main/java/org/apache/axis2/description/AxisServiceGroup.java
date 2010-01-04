be6f77628c642d9363b9ba2c8d641881fc19b2d0
Jason Fager <jason.fager@riskmetrics.com>
2010-01-04 15:52:21 -0500

    convert AxisServiceGroup to interface & implementation

--- sapon-axis2-kernel/src/main/java/org/apache/axis2/description/AxisServiceGroup.java
+++ sapon-axis2-kernel/src/main/java/org/apache/axis2/description/AxisServiceGroup.java
@@ -1,204 +1,32 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-
 package org.apache.axis2.description;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.alt.ModuleConfigAccessor;
+import org.apache.axis2.description.hierarchy.AxisDescription;
+import org.apache.axis2.description.hierarchy.ConfigurationDescendant;
 import org.apache.axis2.engine.AxisConfiguration;
-import org.apache.axis2.engine.AxisEvent;
-import org.apache.axis2.i18n.Messages;
 
-public class AxisServiceGroup extends AxisDescription
-	implements ModuleConfigAccessor
+public interface AxisServiceGroup
+	extends AxisDescription, ConfigurationDescendant, ModuleConfigAccessor
 {
+	void setAxisConfiguration(AxisConfiguration config);
 
-    //to check whether user has put WWW dir or not
-    private boolean foundWebResources;
-    // to store module ref at deploy time parsing
-    private final List<String> modulesList = new ArrayList<String>();
+	void addService(AxisService service) throws AxisFault;
+	AxisService getService(String serviceName);
+	Collection<AxisService> getServices();
+	AxisService removeService(String serviceName);
 
-    // to store modeule configuration info
-    private Map<String, ModuleConfiguration> moduleConfigmap;
+	ClassLoader getServiceGroupClassLoader();
+	void setServiceGroupClassLoader(ClassLoader classLoader);
 
-    // class loader
-    private ClassLoader serviceGroupClassLoader;
+	String getName();
+	void setName(String name);
 
-    // to keep name of the service group
-    private String serviceGroupName;
+	//TODO: these belong somewhere else.
+	List<String> getModuleRefs();
+	void addModuleRef(String ref);
 
-    public AxisServiceGroup() {
-        moduleConfigmap = new HashMap<String, ModuleConfiguration>();
-    }
-
-    public AxisServiceGroup(AxisConfiguration axisDescription) {
-        this();
-        setParent(axisDescription);
-    }
-
-    /**
-     * Adds module configuration, if there is moduleConfig tag in service.
-     *
-     * @param moduleConfiguration the ModuleConfiguration to add
-     */
-    public void addModuleConfig(ModuleConfiguration moduleConfiguration) {
-        if (moduleConfigmap == null) {
-            moduleConfigmap = new HashMap<String, ModuleConfiguration>();
-        }
-
-        moduleConfigmap.put(moduleConfiguration.getModuleName(), moduleConfiguration);
-    }
-
-    public void addModuleref(String moduleref) {
-        modulesList.add(moduleref);
-    }
-
-    public void addService(AxisService service) throws AxisFault {
-        if (service == null) {
-            return;
-        }
-
-        if (serviceGroupName == null) {
-            // setup a temporary name based on the first service under this group
-            serviceGroupName = service.getName();
-        }
-
-        service.setParent(this);
-
-        AxisConfiguration axisConfig = getAxisConfiguration();
-
-        if (axisConfig != null) {
-            for (Iterator<AxisModule> iterator = getEngagedModules().iterator(); iterator.hasNext();) {
-                Object o = iterator.next();
-                AxisModule axisModule;
-                if (o instanceof AxisModule) {
-                    axisModule = (AxisModule) o;
-                } else if (o instanceof String) { //Should this be checked
-                    String moduleName = (String) o;
-                    axisModule = axisConfig.getModule(moduleName);
-                    if (axisModule == null) {
-                        throw new AxisFault(Messages.getMessage("modulenotavailble", moduleName));
-                    }
-                } else {
-                    throw new AxisFault(Messages.getMessage("modulenotavailble"));
-                }
-                service.engageModule(axisModule);
-            }
-
-        }
-        service.setLastUpdate();
-        addChild(service);
-        if (axisConfig != null) {
-            axisConfig.addToAllServicesMap(service);
-        }
-    }
-
-    /**
-     * When a module gets engaged on a ServiceGroup, we have to engage it for each Service.
-     *
-     * @param module the newly-engaged AxisModule
-     * @param engager
-     * @throws AxisFault if there is a problem
-     */
-    @Override
-	protected void onEngage(AxisModule module, AxisDescription engager) throws AxisFault {
-        for (final AxisService axisService: getServices()) {
-            axisService.engageModule(module, engager);
-        }
-    }
-
-    @Override
-	public void onDisengage(AxisModule module) throws AxisFault {
-        for (final AxisService axisService: getServices()) {
-            axisService.disengageModule(module);
-        }
-    }
-
-    public void removeService(String name) throws AxisFault {
-        AxisService service = getService(name);
-
-        if (service != null) {
-            getAxisConfiguration().notifyObservers(AxisEvent.SERVICE_REMOVE, service);
-        }
-
-        removeChild(name);
-    }
-
-    public ModuleConfiguration getModuleConfig(String moduleName) {
-        return moduleConfigmap.get(moduleName);
-    }
-
-    public List<String> getModuleRefs() {
-        return modulesList;
-    }
-
-    public AxisService getService(String name) throws AxisFault {
-        return (AxisService) getChild(name);
-    }
-
-    public ClassLoader getServiceGroupClassLoader() {
-        return serviceGroupClassLoader;
-    }
-
-    public String getServiceGroupName() {
-        // Note: if the serviceGroupName is not set, then this could be null.
-        // If the serviceGroupName has not been set and a service is added to this group,
-        // then the serviceGroupName will default to the name of the first service
-        return serviceGroupName;
-    }
-
-    public Iterable<AxisService> getServices() {
-        return (Iterable<AxisService>)getChildren();
-    }
-
-    public void setAxisDescription(AxisConfiguration axisDescription) {
-        setParent(axisDescription);
-    }
-
-    public void setServiceGroupClassLoader(ClassLoader serviceGroupClassLoader) {
-        this.serviceGroupClassLoader = serviceGroupClassLoader;
-    }
-
-    public void setServiceGroupName(String serviceGroupName) {
-        this.serviceGroupName = serviceGroupName;
-    }
-
-    @Override
-	public Object getKey() {
-        // Note: if the serviceGroupName is not set, then this could be null.
-        // If the serviceGroupName has not been set and a service is added to this group,
-        // then the serviceGroupName will default to the name of the first service
-        return this.serviceGroupName;
-    }
-
-    public boolean isFoundWebResources() {
-        return foundWebResources;
-    }
-
-    public void setFoundWebResources(boolean foundWebResources) {
-        this.foundWebResources = foundWebResources;
-     }
 }
---
+++ sapon-axis2-kernel/src/main/java/org/apache/axis2/description/AxisServiceGroupImpl.java
@@ -0,0 +1,181 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.axis2.description;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.description.hierarchy.AxisDescription;
+import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.axis2.engine.AxisEvent;
+import org.apache.neethi.Policy;
+
+public class AxisServiceGroupImpl extends AxisDescriptionBase
+	implements AxisServiceGroup
+{
+    // to store module ref at deploy time parsing
+    private final List<String> modulesList = new ArrayList<String>();
+
+    private Map<String, ModuleConfiguration> moduleConfigs;
+
+    private Map<String, AxisService> services
+    	= new HashMap<String, AxisService>();
+
+    private ClassLoader serviceGroupClassLoader;
+
+    private String name;
+
+    private AxisConfiguration config;
+
+    public AxisServiceGroupImpl() {
+        moduleConfigs = new HashMap<String, ModuleConfiguration>();
+    }
+
+    public AxisServiceGroupImpl(AxisConfiguration axisConfig) {
+        this();
+        config = axisConfig;
+    }
+
+    public void setAxisConfiguration(AxisConfiguration axisConfig) {
+    	this.config = axisConfig;
+    	this.parameterInclude.setParent(axisConfig);
+    	this.policySubject.setParent(axisConfig);
+    }
+
+    /**
+     * Adds module configuration, if there is moduleConfig tag in service.
+     *
+     * @param moduleConfiguration the ModuleConfiguration to add
+     */
+    public void addModuleConfig(ModuleConfiguration moduleConfiguration) {
+        if (moduleConfigs == null) {
+            moduleConfigs = new HashMap<String, ModuleConfiguration>();
+        }
+
+        moduleConfigs.put(moduleConfiguration.getModuleName(), moduleConfiguration);
+    }
+
+    public void addModuleRef(String moduleref) {
+        modulesList.add(moduleref);
+    }
+
+    public void addService(AxisService service) throws AxisFault {
+        if (service == null) {
+            return;
+        }
+
+        if (name == null) {
+            //use name of first service as default group name
+            name = service.getName();
+        }
+
+        service.setParent(this);
+        for(AxisModule axisModule: getEngagedModules()) {
+            service.engageModule(axisModule);
+        }
+        service.setLastUpdate();
+
+        AxisConfiguration axisConfig = getConfiguration();
+        if (axisConfig != null) {
+            axisConfig.addToAllServicesMap(service);
+        }
+
+        services.put(service.getName(), service);
+    }
+
+    /**
+     * When a module gets engaged on a ServiceGroup, we have to engage it for each Service.
+     *
+     * @param module the newly-engaged AxisModule
+     * @param engager
+     * @throws AxisFault if there is a problem
+     */
+    @Override
+	protected void onEngage(AxisModule module, AxisDescription engager) throws AxisFault {
+        for (final AxisService axisService: getServices()) {
+            axisService.engageModule(module, engager);
+        }
+    }
+
+    @Override
+	public void onDisengage(AxisModule module) throws AxisFault {
+        for (final AxisService axisService: getServices()) {
+            axisService.disengageModule(module);
+        }
+    }
+
+    public AxisService removeService(String name) {
+        AxisService service = services.remove(name);
+        if (service != null) {
+            getConfiguration().notifyObservers(AxisEvent.SERVICE_REMOVE, service);
+        }
+        return service;
+    }
+
+    public ModuleConfiguration getModuleConfig(String moduleName) {
+        return moduleConfigs.get(moduleName);
+    }
+
+    public List<String> getModuleRefs() {
+        return modulesList;
+    }
+
+    public AxisService getService(String name) {
+        return services.get(name);
+    }
+
+    public ClassLoader getServiceGroupClassLoader() {
+        return serviceGroupClassLoader;
+    }
+
+    public String getName() {
+        // Note: if the serviceGroupName is not set, then this could be null.
+        // If the serviceGroupName has not been set and a service is added to this group,
+        // then the serviceGroupName will default to the name of the first service
+        return name;
+    }
+
+    public Collection<AxisService> getServices() {
+        return services.values();
+    }
+
+    public void setServiceGroupClassLoader(ClassLoader serviceGroupClassLoader) {
+        this.serviceGroupClassLoader = serviceGroupClassLoader;
+    }
+
+    public void setName(String serviceGroupName) {
+        this.name = serviceGroupName;
+    }
+
+	@Override
+	public AxisConfiguration getConfiguration() {
+		return config;
+	}
+
+	@Override
+	public Policy getEffectivePolicy() {
+		return getEffectivePolicy(null);
+	}
+}
